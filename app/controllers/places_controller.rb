6b19e51cc0d07801b933a6ef404a0a94bef0204e
Martin McReynolds <martinmcrey@gmail.com>
2019-06-09 18:26:50 +0000

    pagenation issue

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,24 +1,22 @@
 class PlacesController < ApplicationController
 
 def index
-  @places = Place.all
+  @places = Place.all.page(params[:id]).perpage(10)
+end 
 
   def new
      @place = Place.new
   end
+
   def create
     Place.create(place_params)
     redirect_to root_path
   end
-
+  
   private
 
   def place_params
     params.require(:place).permit(:name, :description, :address)
   end
 end
-
-
-# or, use an explicit "per page" limit:
-Places.paginate(page: params[:page], per_page: 10)
 
