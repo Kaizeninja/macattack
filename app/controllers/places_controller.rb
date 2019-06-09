c178938af1f092dee41a215e6e09cfc232ce1072
Martin McReynolds <martinmcrey@gmail.com>
2019-06-09 17:56:22 +0000

    issue

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,8 +1,7 @@
 class PlacesController < ApplicationController
 
 def index
-  @places = Place.all 
-end 
+  @places = Place.all
 
   def new
      @place = Place.new
@@ -18,3 +17,8 @@ end
     params.require(:place).permit(:name, :description, :address)
   end
 end
+
+
+# or, use an explicit "per page" limit:
+Place.paginate(page: params[:page], per_page: 10)
+
