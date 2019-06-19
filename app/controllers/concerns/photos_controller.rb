2aa367f94d73f75fb5951b7501c252bb2b9b86db
Martin McReynolds <martinmcrey@gmail.com>
2019-06-19 02:57:17 +0000

    syntax

--- app/controllers/concerns/photos_controller.rb
+++ app/controllers/concerns/photos_controller.rb
@@ -2,7 +2,6 @@
 class PhotosController < ApplicationController
     before_action :authenticate_user!
 
-    
   def new
     @photo = Photo.new
   end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,8 +1,4 @@
 class Photo < ApplicationRecord
-
   belongs_to :place
-  belongs_to :user
-
-  mount_uploader :picture, PictureUploader
-
+  mount_uploader :Photo, PictureUploader
 end
