2d463e0a1e9281beca580da8ce30495424d04db9
Martin McReynolds <martinmcrey@gmail.com>
2019-06-22 15:41:20 +0000

    work

--- app/controllers/concerns/photos_controller.rb
+++ app/controllers/concerns/photos_controller.rb
@@ -2,21 +2,15 @@
 class PhotosController < ApplicationController
     before_action :authenticate_user!
 
-def index
-end
-
   def create
     @place = Place.find(params[:place_id])
     @place.photos.create(photo_params.merge(user: current_user))
     redirect_to place_path(@place)
-    @photo = Photo.new 
   end
 
   private
 
   def photo_params
-    params.require(:photo).permit(:caption, :picture)
+    params.require(:photo).permit(:picture, :caption)    
   end
-
-
 end
